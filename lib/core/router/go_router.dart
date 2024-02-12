7c19ec7830ca788832f9d86ddaf53103f3c774f4
vbeshta <viacheslavbeshta@gmail.com>
2024-02-12 23:58:59 +0200

    feat: add login screen

--- lib/core/router/go_router.dart
+++ lib/core/router/go_router.dart
@@ -1,13 +1,20 @@
 import 'package:go_router/go_router.dart';
 import 'package:notif/presentation/pages/home/home_screen.dart';
+import 'package:notif/presentation/pages/login/login_layout.dart';
+import 'package:notif/presentation/pages/login/login_screen.dart';
 
 final GoRouter goRouter = GoRouter(
   observers: [],
   routes: <RouteBase>[
     GoRoute(
       path: '/',
-      builder: (_, __) => const HomeScreen(),
+      builder: (_, __) => const LoginLayout(),
       routes: <RouteBase>[
+        GoRoute(
+          name: LoginScreen.routeName,
+          path: LoginScreen.routeName,
+          builder: (_, __) => const LoginLayout(),
+        ),
         GoRoute(
           name: HomeScreen.routeName,
           path: HomeScreen.routeName,
